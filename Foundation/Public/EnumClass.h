2f74850b2aa775d9ccb9c12b8b88d43ef25ce908
Toby Chen <chenyanjun912@hotmail.com>
2019-02-25 01:04:41 -0800

    Even better EnumClass.h

--- Foundation/Public/EnumClass.h
+++ Foundation/Public/EnumClass.h
@@ -13,14 +13,23 @@ inline constexpr Enum operator~(Enum e) { return (Enum)~(std::underlying_type_t<
 
 #define TO_UNDERLYING(T, x) static_cast<typename std::underlying_type<T>::type>(x)
 
+// https://stackoverflow.com/questions/15586163/c11-type-trait-to-differentiate-between-enum-class-and-regular-enum
+template <typename E>
+using is_scoped_enum = std::integral_constant<
+    bool,
+    std::is_enum<E>::value && !std::is_convertible<E, int>::value>;
+
+template<typename E>
+inline constexpr bool is_scoped_enum_v = is_scoped_enum<E>::value;
+
 template <typename enum_t>
-inline std::enable_if_t<std::is_enum_v<enum_t>, bool> Any(enum_t flags)
+inline std::enable_if_t<is_scoped_enum_v<enum_t>, bool> Any(enum_t flags)
 {
 	return TO_UNDERLYING(enum_t, flags) != 0;
 }
 
 template <typename enum_t>
-inline std::enable_if_t<std::is_enum_v<enum_t>, bool> Any(enum_t flags, enum_t compare)
+inline std::enable_if_t<is_scoped_enum_v<enum_t>, bool> Any(enum_t flags, enum_t compare)
 {
 	return (TO_UNDERLYING(enum_t, flags) & TO_UNDERLYING(enum_t, compare)) != 0;
 }

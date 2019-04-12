b7652a5c9655e975ee2c38a7a042c8bf784f0535
Toby Chen <chenyanjun912@hotmail.com>
2019-04-12 02:43:52 -0700

    Remove Foundation and RHI to convert them into submodules

--- Foundation/Public/ThreadName.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "FoundationAPI.h"
-#include <thread>
-
-void FOUNDATION_API SetThreadName(const char* threadName);
-void FOUNDATION_API SetThreadName(std::thread* thread, const char* threadName);

cd0f362f7fe983880ce41b0ec7c17fa236ff0d2f
Toby Chen <chenyanjun912@hotmail.com>
2019-04-26 15:55:06 -0700

    Comment Scene.h

--- Application/Scene/Scene.h
+++ Application/Scene/Scene.h
@@ -32,15 +32,21 @@ public:
 
     void CreateDefaultCamera();
 
+    // Adds an entity into the lookup map
     void AddEntity(TAutoPtr<CEntity> entity);
     //TODO:
     //void RemoveEntity(const std::string& name);
     //void RenameEntity(const std::string& oldName, const std::string& newName);
+    
+    // Finds an entity by its name
     TAutoPtr<CEntity> FindEntity(const std::string& name) const;
 
+    // Creates a group that is represented by a scene node with the specified name
     TAutoPtr<CSceneNode> CreateGroup(const std::string& name);
+    // Finds a group by its name
     TAutoPtr<CSceneNode> FindGroup(const std::string& name) const;
 
+    // Locate in the scene a point output (could be a point or a mesh vertex) by its path
     Flow::TOutput<CVertexInfo*>* FindPointOutput(const std::string& id) const;
 
     ///Walks the scene tree along a path, return the last matching node and the rest of path
@@ -62,8 +68,11 @@ public:
 private:
     CBankAndSet BankAndSet;
 
+    // This is the root node of the scene tree
     TAutoPtr<CSceneNode> RootNode;
+    // This points to the scene node that is associated with the current camera
     TAutoPtr<CSceneNode> CameraNode;
+    // The main camera that contains projection parameters etc.
     TAutoPtr<CCamera> MainCamera;
     CViewport* Viewport = nullptr;
     tc::Color ClearColor;
@@ -73,6 +82,8 @@ private:
 
     TAutoPtr<CPickingManager> PickingMgr;
 
+    // Every generator or group in the nom file is declared with a name
+    // The following two maps enable looking up objects by their names
     std::map<std::string, TAutoPtr<CEntity>> EntityLibrary;
     std::map<std::string, TAutoPtr<CSceneNode>> Groups;
 };

23f68ffc9733a87e5288c101d86632ae52fb22aa
Toby Chen <chenyanjun912@hotmail.com>
2019-10-08 00:56:29 -0700

    Add debug draw interface

---
+++ Application/QtFrontend/DebugDraw.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <Scene/RendererInterface.h>
+#include <Qt3DRender/Qt3DRender>
+
+namespace Nome
+{
+
+class CDebugDraw : public Qt3DCore::QNode, public IDebugDraw
+{
+public:
+    explicit CDebugDraw(Qt3DCore::QNode* parent = nullptr);
+
+    void Reset();
+    void Commit();
+
+    [[nodiscard]] Qt3DRender::QGeometry* GetLineGeometry() const { return LineGeometry.get(); }
+
+    ///Draw fresh primitives every frame, fairly costly
+    void DrawPoint(tc::Vector3 pos, tc::Color color) override;
+    void LineSegment(tc::Vector3 from, tc::Color fromColor, tc::Vector3 to, tc::Color toColor) override;
+
+private:
+    QByteArray PointData;
+    QByteArray LineData;
+    Qt3DRender::QBuffer* PointBuffer;
+    Qt3DRender::QBuffer* LineBuffer;
+    std::unique_ptr<Qt3DRender::QGeometry> LineGeometry;
+};
+
+}
--- Application/Scene/RendererInterface.h
+++ Application/Scene/RendererInterface.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <Vector3.h>
+#include <Color.h>
 
 namespace Nome
 {
@@ -19,6 +21,16 @@ public:
 
 protected:
     ~IMeshRenderer() = default;
+};
+
+class IDebugDraw
+{
+public:
+    virtual void DrawPoint(tc::Vector3 pos, tc::Color color) = 0;
+    virtual void LineSegment(tc::Vector3 from, tc::Color fromColor, tc::Vector3 to, tc::Color toColor) = 0;
+
+protected:
+    ~IDebugDraw() = default;
 };
 
 }

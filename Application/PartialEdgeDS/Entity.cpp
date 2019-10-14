dec7494185ab07407089bff85471aa25896ca2dd
Peter Generao <pgenerao516@gmail.com>
2019-10-14 13:15:44 -0700

    Add Entity class and definition for PartialEdgeDS.

---
+++ Application/PartialEdgeDS/Entity.cpp
@@ -0,0 +1,5 @@
+//
+// Created by peter on 10/14/19.
+//
+
+#include "Entity.h"
---
+++ Application/PartialEdgeDS/Entity.h
@@ -0,0 +1,15 @@
+//
+// Created by peter on 10/14/19.
+//
+
+#ifndef NOME3_ENTITY_H
+#define NOME3_ENTITY_H
+
+
+class Entity
+{
+
+};
+
+
+#endif //NOME3_ENTITY_H

953a216eadffdaf053c5ed494d8380db9da16bbd
Peter Generao <pgenerao516@gmail.com>
2019-10-14 00:56:42 -0700

    Add Partial Edge Data Structure header (Accidentally didn't add it last time).

---
+++ Application/Scene/PartialEdgeDS.h
@@ -0,0 +1,117 @@
+//
+// Created by peter on 10/13/19.
+//
+#pragma once
+#include <memory>
+
+namespace Nome::Scene::PartialEdgeDS
+{
+
+/**
+ * The Partial Edge Data Structure that we will be implementing will closely
+ * correspond to the paper "Partial Entity Structure: A Compact Non-Manifold
+ * Boundary Representation Based on Partial Topological Entities", by Sang Hun
+ * Lee and Kunwoo Lee; however, we will be enforcing a strict linked list
+ * structure. The first two "
+ * 
+*/
+
+class Entity
+{
+public:
+    int id;
+};
+
+class Model: public Entity
+{
+public:
+    std::shared_ptr<Model> next;
+    std::shared_ptr<Region> region;
+};
+
+class Region: public Entity
+{
+public:
+    std::shared_ptr<Model> model;
+    std::shared_ptr<Region> next;
+    std::shared_ptr<Shell> shell;
+};
+
+class Shell: public Entity
+{
+public:
+    std::shared_ptr<Region> region;
+    std::shared_ptr<Shell> next;
+    std::shared_ptr<PFace> pface;
+};
+
+class PFace: public Entity
+{
+public:
+    std::shared_ptr<Region> region;
+    std::shared_ptr<PFace> next;
+    std::shared_ptr<Entity> child; // Can be a face, edge, or vertex
+
+    // Extra Information
+    //TODO: Add Orientation Flag
+    std::shared_ptr<PFace> mate;
+};
+
+class Face: public Entity
+{
+public:
+    std::shared_ptr<PFace> pface;
+    std::shared_ptr<Face> next;
+    std::shared_ptr<Loop> loop;
+};
+
+class Loop: public Entity
+{
+public:
+    std::shared_ptr<Face> face;
+    std::shared_ptr<Loop> next;
+    std::shared_ptr<PEdge> pedge;
+};
+
+class PEdge: public Entity
+{
+public:
+    std::shared_ptr<Loop> loop;
+    std::shared_ptr<PEdge> next;
+    std::shared_ptr<Entity> child; // Can be a edge, or vertex
+
+    // Extra Information
+    //TODO: Add Orientation Flag
+    std::shared_ptr<PVertex> pvertex; // Start pvertex
+    std::shared_ptr<PEdge> looped_prev;
+    std::shared_ptr<PEdge> looped_next;
+    std::shared_ptr<PEdge> radial_prev;
+    std::shared_ptr<PEdge> radial_next;
+};
+
+class Edge: public Entity
+{
+public:
+    std::shared_ptr<Entity> parent; // Can be a partial face, or partial edge
+    std::shared_ptr<Edge> next;
+    std::shared_ptr<PVertex> pvertex;
+};
+
+class PVertex: public Entity
+{
+public:
+    std::shared_ptr<Edge> edge;
+    std::shared_ptr<PVertex> next;
+    std::shared_ptr<Vertex> vertex;
+};
+
+class Vertex: public Entity
+{
+public:
+    std::shared_ptr<Entity> parent; // Can be a partial face, partial edge,
+                                    // or partial vertex
+    std::shared_ptr<Vertex> next;
+    //TODO: Add geometry for single point
+};
+
+}

e9a44eaa6aaae4720ba6cca493ace78ddf7cd5ce
zxYin <yinzxpersonal@gmail.com>
2019-11-01 09:24:20 -0700

    add geometry file

---
+++ Application/PartialEdgeDS/Geometry.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 殷子欣 on 2019/11/1.
+//
+
+#include "Geometry.h"
---
+++ Application/PartialEdgeDS/Geometry.h
@@ -0,0 +1,12 @@
+//
+// Created by 殷子欣 on 2019/11/1.
+//
+
+#ifndef NOME3_GEOMETRY_H
+#define NOME3_GEOMETRY_H
+
+class Geometry
+{
+};
+
+#endif // NOME3_GEOMETRY_H

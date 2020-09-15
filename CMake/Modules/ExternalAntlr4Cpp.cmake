1bb9b7fc099891c05e3bada60f32345b8cc7025d
Randy Fan <randyfan@berkeley.edu>
2020-09-15 16:07:59 -0700

    Potentially fixed the annoying ANTLR bug

--- CMake/Modules/ExternalAntlr4Cpp.cmake
+++ CMake/Modules/ExternalAntlr4Cpp.cmake
@@ -2,9 +2,14 @@ cmake_minimum_required(VERSION 3.7)
 
 include(ExternalProject)
 
-set(ANTLR4_ROOT ${CMAKE_CURRENT_BINARY_DIR}/antlr4_runtime/src/antlr4_runtime)
-set(ANTLR4_INCLUDE_DIRS ${ANTLR4_ROOT}/runtime/Cpp/runtime/src)
-set(ANTLR4_GIT_REPOSITORY https://github.com/antlr/antlr4.git)
+set(ANTLR4_ROOT ${CMAKE_CURRENT_BINARY_DIR}/antlr4_runtime/src/antlr4_runtime) #9/15
+set(ANTLR4_INCLUDE_DIRS ${ANTLR4_ROOT}/runtime/Cpp/runtime/src) #9/15
+set(ANTLR4_GIT_REPOSITORY https://github.com/randyfan/oldANTLR.git) #9/15 attempt to change to local
+
+#set(ANTLR_EXECUTABLE ${PROJECT_SOURCE_DIR}/thirdparty/antlr-4.8-complete.jar)
+#set(ANTLR4_ZIP_REPOSITORY ${PROJECT_SOURCE_DIR}/thirdparty/antlr4-827_old.zip) #https://dbgit.prakinf.tu-ilmenau.de/code/poseidon/commit/813152339a6c0e428292fe75c4d498b8016445da
+
+
 if(NOT DEFINED ANTLR4_TAG)
   # Set to branch name to keep library updated at the cost of needing to rebuild after 'clean'
   # Set to commit hash to keep the build stable and does not need to rebuild after 'clean'

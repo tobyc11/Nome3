f0829c206888c360f35656faa2888b1d86d64406
Randy Fan <randyfan@berkeley.edu>
2020-10-10 22:29:28 -0700

    Reverting NOME3DView to few days ago due to continued bugs

--- Application/QtFrontend/Nome3DView.h
+++ Application/QtFrontend/Nome3DView.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "DebugDraw.h"
 #include "InteractiveMesh.h"
-#include "OrbitTransformController.h"
 #include <Ray.h>
 #include <Scene/Scene.h>
 
@@ -30,16 +29,9 @@ public:
     void PostSceneUpdate();
 
     void PickVertexWorldRay(const tc::Ray& ray);
+    void PickFaceWorldRay(const tc::Ray& ray); // Randy added on 10/10
 
     static Qt3DCore::QEntity* MakeGridEntity(Qt3DCore::QEntity* parent);
-
-protected:
-    // Xinyu added on Oct 8 for rotation
-    void mouseMoveEvent(QMouseEvent* e) override;
-    void mousePressEvent(QMouseEvent* e) override;
-    void mouseReleaseEvent(QMouseEvent* e) override;
-    void wheelEvent(QWheelEvent *ev) override;
-    void keyPressEvent(QKeyEvent *ev) override;
 
 private:
     Qt3DCore::QEntity* Root;
@@ -47,25 +39,6 @@ private:
     std::unordered_set<CInteractiveMesh*> InteractiveMeshes;
     std::unordered_map<Scene::CEntity*, CDebugDraw*> EntityDrawData;
     std::vector<std::string> SelectedVertices;
-
-    // Xinyu added on Oct 8 for rotation
-    QMatrix4x4 projection;
-    QVector2D mousePressPosition;
-    QVector3D rotationAxis;
-    qreal angularSpeed;
-    QQuaternion rotation;
-    Qt3DRender::QCamera *cameraset;
-    Qt3DExtras::QOrbitCameraController *camController;
-    //Qt3DRender::QMaterial *material;
-    bool mousePressEnabled;
-    bool animationEnabled;
-    float zPos;
-
-    // For the animation
-    Qt3DCore::QTransform *sphereTransform;
-    OrbitTransformController *controller;
-    QPropertyAnimation *sphereRotateTransformAnimation;
-
 };
 
 }
